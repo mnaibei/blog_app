5e186d477351ecde06da2ba1c29d9adb3726d9ab
mnaibei <muchajulius@gmail.com>
2023-07-18 15:18:39 +0300

    Made required changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,4 @@ class UsersController < ApplicationController
   def show
     @users = User.find(params[:id])
   end
-
-  # def posts
-  #   # @user = User.find(params[:user_id])
-  #   # @posts = @user.recent_posts
-  # end
-
-  # def show_post
-  #   @user = User.find(params[:user_id])
-  #   @post = Post.find(params[:id])
-  # end
 end
