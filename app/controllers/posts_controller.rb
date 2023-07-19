db4fc9413915a338d80b7fe301d9a22dfacbf6c0
mnaibei <muchajulius@gmail.com>
2023-07-19 13:36:21 +0300

    Added posts form and user partial

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,27 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @user = @post.author
   end
+
+  def new
+    @post = Post.new
+    @user = User.find(params[:user_id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @user = User.find(params[:user_id])
+    @post.author = @user
+    if @post.save
+      redirect_to user_post_url(@user, @post)
+    else
+      flash.now[:errors] = @post.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: %i[index show new create] do
+      resources :comments, only: %i[new create]
+    end
   end
 end
