be841e8241d28a963cdf6d2b75e76352e8698e2a
fatiha <fathiato@gmail.com>
2023-07-26 09:14:09 +0100

    made requested changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: %i[index show]
   def index
     # @posts = Post.all
     @user = User.find(params[:user_id])
